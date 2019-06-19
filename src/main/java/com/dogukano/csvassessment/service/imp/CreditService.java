a246d468704c14ae8deea29cde5f193ece1c27e3
Doğukan Öztürkan <d.ozturkan@gmail.com>
2019-06-19 20:39:43 +0300

    SBR-3 Credit / Debit Service Updated

--- src/main/java/com/dogukano/csvassessment/service/imp/CreditService.java
+++ src/main/java/com/dogukano/csvassessment/service/imp/CreditService.java
@@ -1,18 +1,37 @@
 package com.dogukano.csvassessment.service.imp;
 
+import com.dogukano.csvassessment.model.Credit;
 import com.dogukano.csvassessment.model.reports.CreditReport;
+import com.dogukano.csvassessment.repository.CreditRepository;
 import com.dogukano.csvassessment.service.adapter.TransactionAdapter;
 import com.google.common.collect.Range;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreditService implements TransactionAdapter<CreditReport> {
 
+    @Autowired
+    CreditRepository creditRepository;
+
     @Override
     public CreditReport getReport(Range<Date> range) {
-        return null;
+
+        List<Credit> credits = creditRepository
+                .findByDateSentBetween(
+                        range.lowerEndpoint(),
+                        range.upperEndpoint());
+        credits.toString();
+        return CreditReport.builder()
+                .credits(
+                        creditRepository.findByDateSentBetween(
+                                range.lowerEndpoint(),
+                                range.upperEndpoint())
+                )
+                .build();
     }
 
     @Override
--- src/main/java/com/dogukano/csvassessment/service/imp/DebitService.java
+++ src/main/java/com/dogukano/csvassessment/service/imp/DebitService.java
@@ -1,18 +1,30 @@
 package com.dogukano.csvassessment.service.imp;
 
 import com.dogukano.csvassessment.model.reports.DebitReport;
+import com.dogukano.csvassessment.repository.DebitRepository;
 import com.dogukano.csvassessment.service.adapter.TransactionAdapter;
 import com.google.common.collect.Range;
 import java.math.BigDecimal;
 import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DebitService implements TransactionAdapter<DebitReport> {
 
+    @Autowired
+    DebitRepository debitRepository;
+
     @Override
     public DebitReport getReport(Range<Date> range) {
-        return null;
+
+        return DebitReport.builder()
+                .debits(
+                        debitRepository.findByDateBetween(
+                                range.lowerEndpoint(),
+                                range.upperEndpoint())
+                )
+                .build();
     }
 
     @Override
